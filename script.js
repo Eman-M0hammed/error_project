15668ccce0ad120513dfc4fdf02d80bf9031c8a5
MohamedEltohamyAly <88947139+MohamedEltohamyAly@users.noreply.github.com>
2021-09-01 23:52:06 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 // write code here
+$(document).ready(function(){
+    $('.slider').slick({
+        autoplay: true,
+        dots: true,
+        infinite: true,
+        
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+})
